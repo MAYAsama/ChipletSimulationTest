8d60b46dd2619b02b3dbeec8d3fa6ac7ad6f9d45
MAYAsama <670242395@qq.com>
2022-05-18 10:22:09 +0800

    modify

--- SPsim/run1.sh
+++
@@ -1,1 +0,0 @@
-(valgrind --tool=memcheck --leak-check=full --show-leak-kinds=all ./booksim examples/8x8MeshAsync chip1 chip2 chip3 chip4) > memcheck
--- SPsim/run2.sh
+++
@@ -1,1 +0,0 @@
-MALLOC_CHECK_=1 ./booksim interposer chip1 chip2 chip3 chip4 > testbench_1
